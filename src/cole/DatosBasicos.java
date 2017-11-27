409d6bbe009f6882e7116204f6bf87c108153ef8
jdominguezdominguez <jdominguezdominguez@victoria28.danielcastelao.org>
2017-11-27 10:01:19 +0100

    Cole Commit final

---
+++ src/cole/DatosBasicos.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cole;
+
+/**
+ *
+ * @author jdominguezdominguez
+ */
+public class DatosBasicos {
+
+    private String nome, direccion;
+
+    public DatosBasicos(String nome, String direccion) {
+        this.nome = nome;
+        this.direccion = direccion;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    @Override
+    public String toString() {
+        return "nome=" + nome + ", direccion=" + direccion;
+    }
+
+}
